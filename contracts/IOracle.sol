af9505e7a5f0b21dc933ecab3d12a7a366f1958a
Oluwaseun Adebambo <seun.bambo@gmail.com>
2021-03-04 18:57:15 +0100

    Oracles

---
+++ contracts/IOracle.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.7.3;
+
+interface IOracle {
+    function getData(bytes32 key)
+        external
+        view
+        returns (
+            bool result,
+            uint256 date,
+            uint256 payload
+        );
+}
